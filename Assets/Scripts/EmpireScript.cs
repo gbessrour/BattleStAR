09d7e691775899a1f4e627fc8e0ab9f838cd6d40
gbessrour <ghassenbessrour@gmail.com>
2020-02-02 02:23:47 -0500

    Prepared script for victory counter and message

---
+++ Assets/Scripts/EmpireScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Slider slider;
+    private float health = 0f;
+    int counter = 0;
+    bool victory = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        while (counter < 3)
+        {
+            if (collision.collider.tag == "Bullet1")
+            {
+                Destroy(collision.gameObject);
+                health += .1f;
+                slider.value = health;
+                counter++;
+            }
+        }
+        victory = true;
+        SceneManager.LoadScene(0);
+    }
+
+}
